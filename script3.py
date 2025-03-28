d7120f4a1f6791b4daba82d5d948eafa6dc0adfb
hdahhaoui <d_hachimi@yahoo.com>
2025-03-29 00:56:53 +0100

    Update script3.py

--- script3.py
+++ script3.py
@@ -6,8 +6,10 @@ import pandas as pd
 import altair as alt
 
 # Clés API (doivent être configurées dans les secrets de l'application Streamlit)
-DEEPSEEK_API_KEY = st.secrets.get("DEEPSEEK_API_KEY", None)
-OWM_API_KEY = st.secrets.get("OWM_API_KEY", None)
+deepseek_key = st.secrets["DEEPSEEK"]["API_KEY"]
+owm_key = st.secrets["OWM"]["API_KEY"]
+#DEEPSEEK_API_KEY = st.secrets.get("DEEPSEEK_API_KEY", None)
+#OWM_API_KEY = st.secrets.get("OWM_API_KEY", None)
 
 # Tarif de l'électricité (DZD par kWh) - constant
 TARIF_ELECTRICITE = 5  # 5 DZD/kWh
